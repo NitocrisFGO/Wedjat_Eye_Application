e400de8d479859e0a74b96358d650564ef821e55
NitocrisFGO <ycy20011234@gmail.com>
2024-11-22 14:23:36 +0800

    ready for deploying for now.

--- WedjatEye.py
+++ WedjatEye.py
@@ -164,5 +164,5 @@ def processed_file(filename):
 # start server
 if __name__ == "__main__":
     colorizer_eccv16, colorizer_siggraph17 = load_colorization_models()
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
